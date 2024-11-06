3c4d4eb3d9d5c8036a22fccd608fc7b2d74bd42f
OmarSabil <omar.sabil.official01@gmail.com>
2024-11-06 19:08:57 +0600

    added total price

--- src/Components/Mid/Dashboard/CartList.jsx
+++ src/Components/Mid/Dashboard/CartList.jsx
@@ -7,23 +7,28 @@ import { SlidersVertical } from "lucide-react";
 const CartList = () => {
     const allProducts = useLoaderData()
     const [cartList, setCartList] = useState([])
+    const [previousPrice, setPreciousPrice] = useState(0)
 
     useEffect(() => {
         const storedCartList = getStoredCart()
         const storedCartListInt = storedCartList.map(id => parseInt(id));
 
         const cartProductList = allProducts.filter(product => storedCartList.includes(product.product_id))
-
         setCartList(cartProductList);
+
+        const totalCost = cartProductList.reduce((previousPrice, cart) => previousPrice + Number(cart.price), 0);
+        setPreciousPrice(totalCost)
+        console.log()
     }, [])
-    console.log(cartList)
+    
     return (
         <div className="">
             {/* section top */}
             <div className="px-10 flex justify-between items-center">
                 <h3 className="text-xl font-bold">Cart List</h3>
                 <div className="flex items-center gap-20">
-                    <h3 className="text-xl font-bold">Total Cost: {0}</h3>
+                    <h3 className="text-xl font-bold">Total Cost: {previousPrice}</h3>
+                    {/* buttons */}
                     <div className="flex items-center my-5 gap-2">
                         <button className="btn border border-purple-500 text-purple-600 rounded-full hover:bg-purple-600 hover:text-white">Sort with Price <SlidersVertical/></button>
                         <button className="btn bg-gradient-to-b from-purple-600 to-pink-700 text-white rounded-full hover:border hover:border-purple-500 hover:text-purple-600 hover:bg-none hover:bg-purple-200">Purchase</button>
