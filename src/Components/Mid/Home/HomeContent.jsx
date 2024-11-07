ff7f04d4fdef47777a4a7412ee3abf857fb15daa
OmarSabil <omar.sabil.official01@gmail.com>
2024-11-07 09:27:01 +0600

    recreated categories

--- src/Components/Mid/Home/HomeContent.jsx
+++ src/Components/Mid/Home/HomeContent.jsx
@@ -2,7 +2,6 @@ import { useLoaderData } from 'react-router-dom';
 import HomeBanner from './HomeBanner'
 import Gadget from './Gadget';
 import { useState } from 'react';
-import { Smartphone } from 'lucide-react';
 
 const HomeContent = () => {
     const [selectedCategory, setSelectedCategory] = useState('all')
@@ -20,13 +19,13 @@ const HomeContent = () => {
             <h3 className='text-3xl font-bold w-fit mx-auto mb-10'>Explore Cutting-Edge Gadgets</h3>
             <section className='flex gap-5'>
                 <div>
-                <div className='flex flex-col w-fit gap-2 p-2 border border-black rounded-2xl h-fit sticky top-0'>
-                    <button onClick={() => setSelectedCategory('all')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>All</button>
-                    <button onClick={() => setSelectedCategory('Smartphone')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>Smartphone</button>
-                    <button onClick={() => setSelectedCategory('Laptop')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>Laptop</button>
-                    <button onClick={() => setSelectedCategory('TV')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>Smart TV</button>
-                    <button onClick={() => setSelectedCategory('Smart Watch')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>Smart Watch</button>
-                    <button onClick={() => setSelectedCategory('Accessories')} className='btn btn-xs  md:btn-sm w-full border border-black rounded-full'>Accessories</button>
+                <div className='flex flex-col w-fit gap-3 p-4 border border-purple-400 bg-gray-100 rounded-2xl h-fit sticky top-0'>
+                    <button onClick={() => setSelectedCategory('all')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>All</button>
+                    <button onClick={() => setSelectedCategory('Smartphone')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>Smartphone</button>
+                    <button onClick={() => setSelectedCategory('Laptop')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>Laptop</button>
+                    <button onClick={() => setSelectedCategory('TV')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>Smart TV</button>
+                    <button onClick={() => setSelectedCategory('Smart Watch')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>Smart Watch</button>
+                    <button onClick={() => setSelectedCategory('Accessories')} className='btn btn-xs  md:btn-sm w-full border border-purple-500 text-purple-600 bg-purple-50 hover:text-white hover:bg-purple-600 hover:border-purple-600 rounded-full'>Accessories</button>
                 </div>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
